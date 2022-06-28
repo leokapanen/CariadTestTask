ee3c18910af636176091c3e66c8aab715ebf6743
Leonid Kapanen <leokapanen@gmail.com>
2022-06-28 23:56:10 +0300

    Unused permissions were removed.

--- app/src/main/java/com/kapanen/cariadtesttask/MainActivity.kt
+++ app/src/main/java/com/kapanen/cariadtesttask/MainActivity.kt
@@ -3,8 +3,6 @@ package com.kapanen.cariadtesttask
 import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.os.StrictMode
-import android.os.StrictMode.ThreadPolicy
 import android.preference.PreferenceManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -14,7 +12,6 @@ import androidx.navigation.findNavController
 import com.kapanen.cariadtesttask.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 import org.osmdroid.config.Configuration
-
 
 private const val MULTIPLE_PERMISSION_REQUEST_CODE = 4
 
@@ -45,41 +42,17 @@ class MainActivity : AppCompatActivity() {
             this,
             Manifest.permission.INTERNET
         )
-        val networkStatePermissionCheck: Int = ContextCompat.checkSelfPermission(
-            this,
-            Manifest.permission.ACCESS_NETWORK_STATE
-        )
         val writeExternalStoragePermissionCheck: Int = ContextCompat.checkSelfPermission(
             this,
             Manifest.permission.WRITE_EXTERNAL_STORAGE
         )
-        val coarseLocationPermissionCheck: Int = ContextCompat.checkSelfPermission(
-            this,
-            Manifest.permission.ACCESS_COARSE_LOCATION
-        )
-        val fineLocationPermissionCheck: Int = ContextCompat.checkSelfPermission(
-            this,
-            Manifest.permission.ACCESS_FINE_LOCATION
-        )
-        val wifiStatePermissionCheck: Int = ContextCompat.checkSelfPermission(
-            this,
-            Manifest.permission.ACCESS_WIFI_STATE
-        )
         if (internetPermissionCheck != PackageManager.PERMISSION_GRANTED
-            && networkStatePermissionCheck != PackageManager.PERMISSION_GRANTED
             && writeExternalStoragePermissionCheck != PackageManager.PERMISSION_GRANTED
-            && coarseLocationPermissionCheck != PackageManager.PERMISSION_GRANTED
-            && fineLocationPermissionCheck != PackageManager.PERMISSION_GRANTED
-            && wifiStatePermissionCheck != PackageManager.PERMISSION_GRANTED
         ) {
             ActivityCompat.requestPermissions(
                 this, arrayOf(
                     Manifest.permission.INTERNET,
-                    Manifest.permission.ACCESS_NETWORK_STATE,
                     Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                    Manifest.permission.ACCESS_COARSE_LOCATION,
-                    Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.ACCESS_WIFI_STATE
                 ),
                 MULTIPLE_PERMISSION_REQUEST_CODE
             )
